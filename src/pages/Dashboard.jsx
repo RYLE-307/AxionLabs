40ac12cc11699b2a3fd3180bac6518fac1249168
maks <makskozlov140@gmail.com>
2025-09-21 12:26:45 +0300

    123
    123

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -531,7 +531,7 @@ const runTestRun = (testRunId) => {
                             className="btn btn-sm btn-outline" 
                             onClick={() => runTestRun(testRun.id)}
                           >
-                            Запустит
+                            Запустить
                           </button>
                         ) : (
                           <button className="btn btn-sm btn-outline" disabled>
